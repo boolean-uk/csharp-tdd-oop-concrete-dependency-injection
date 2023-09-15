fc2b9cc7e9d8bad8a34b0dc308ee1328403317a3
Ntina B <n_bekiari@outlook.com>
2023-09-15 20:11:13 +0300

    ok .

--- tdd-oop-concrete-dependency-injection.CSharp.Main/Game.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Main/Game.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace tdd_oop_concrete_dependency_injection.CSharp.Main
 {
-    public class Game 
+    public class Game
     {
         public string name;
 
-        public Game(string name) {
-           
+        public Game(string name)
+        {
+            this.name = name; 
         }
 
-        public String start() {
+        public string Start()
+        {
             return "Playing " + this.name;
         }
     }
