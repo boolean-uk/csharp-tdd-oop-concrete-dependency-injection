1c3fb00e02afe6d6bf522f11649c345c03844f79
Annefleur Hay <annefleurhay1@gmail.com>
2023-09-15 14:32:25 +0200

    added core

--- tdd-oop-concrete-dependency-injection.CSharp.Main/Computer.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Main/Computer.cs
@@ -14,11 +14,21 @@ namespace tdd_oop_concrete_dependency_injection.CSharp.Main
 
         public Computer(PowerSupply powerSupply) {
             this.powerSupply = powerSupply;
+            //this.installedGames.AddRange(preInstalled);
         }
+        public Computer(PowerSupply powerSupply, List<Game> preInstalled)
+        {
+            this.powerSupply = powerSupply;
+            this.installedGames.AddRange(preInstalled);
+        }
+        
+
+        
 
         public void turnOn() {
-            PowerSupply psu = new PowerSupply();
-            psu.turnOn();
+            //PowerSupply psu = new PowerSupply();
+            //psu.turnOn();
+            this.powerSupply.turnOn();
         }
 
         public void installGame(string name) {
@@ -26,11 +36,12 @@ namespace tdd_oop_concrete_dependency_injection.CSharp.Main
             this.installedGames.Add(game);
         }
 
-        public String playGame(string name) {
+        public string playGame(string name) {
             foreach (Game g in this.installedGames) {
                 if (g.name.Equals(name)) {
                     return g.start();
                 }
+                
             }
 
             return "Game not installed";
