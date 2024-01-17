b9ee44bcfbc357783ab7022f8343aff0db59770e
Victor <victor.adamson@se.experis.com>
2024-01-17 14:26:27 +0100

    Solved core tasks

--- tdd-oop-concrete-dependency-injection.CSharp.Main/Program.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Main/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tdd_oop_concrete_dependency_injection.CSharp.Main;
+
+PowerSupply myPsu = new PowerSupply();
+Computer myPc = new Computer(myPsu);
+
+//myPc.installGame("Final Fantasy XI");
+
+
+List<Game> preInstalled = new List<Game>();
+myPc.installGame("Dark souls 2");
+myPc.playGame("Dark souls 2");
+preInstalled.Add(new Game("Dwarf Fortress"));
+preInstalled.Add(new Game("Baldur's Gate"));
+Console.WriteLine("");
