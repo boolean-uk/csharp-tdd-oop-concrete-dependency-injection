0bbcd45d1cd5d2793066b0b52bcc8357420d15fc
Gudbrand <gudbrandwd@live.no>
2024-01-18 14:38:48 +0100

    done

--- tdd-oop-concrete-dependency-injection.CSharp.Main/Game.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Main/Game.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace tdd_oop_concrete_dependency_injection.CSharp.Main;
 
-namespace tdd_oop_concrete_dependency_injection.CSharp.Main
+public class Game 
 {
-    public class Game 
-    {
-        public string name;
+    public string name;
 
-        public Game(string name) {
-           
-        }
+    public Game(string name) {
+       this.name = name;
+    }
 
-        public String start() {
-            return "Playing " + this.name;
-        }
+    public String start() {
+        return "Playing " + this.name;
     }
 }
