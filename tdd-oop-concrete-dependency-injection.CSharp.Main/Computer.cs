ded842775a55cc0bb9de64c18af44ca0ff63fbe0
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-19 09:47:34 +0100

    Core done

--- tdd-oop-concrete-dependency-injection.CSharp.Main/Computer.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Main/Computer.cs
@@ -14,11 +14,16 @@ namespace tdd_oop_concrete_dependency_injection.CSharp.Main
 
         public Computer(PowerSupply powerSupply) {
             this.powerSupply = powerSupply;
+
+        }
+        public Computer(PowerSupply powerSupply, List<Game> preInstalled)
+        {
+            this.powerSupply = powerSupply;
+            this.installedGames = preInstalled;
         }
 
         public void turnOn() {
-            PowerSupply psu = new PowerSupply();
-            psu.turnOn();
+            powerSupply.turnOn();
         }
 
         public void installGame(string name) {
