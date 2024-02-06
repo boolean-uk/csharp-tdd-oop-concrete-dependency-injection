e6401222c1f88682eee6f346e3957bda5b54d8ce
Rose van der Giessen <Rose van der Giessen>
2024-02-06 15:01:08 +0100

    Extension complete

--- tdd-oop-concrete-dependency-injection.CSharp.Test/ComputerTest.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Test/ComputerTest.cs
@@ -20,8 +20,9 @@ namespace tdd_oop_concrete_dependency_injection.CSharp.Test
         {
             PowerSupply myPsu = new PowerSupply();
             Computer myPc = new Computer(myPsu);
+            Game game = new Game("Final Fantasy XI");
 
-            myPc.installGame("Final Fantasy XI");
+            myPc.installGame(game);
 
             int timesFFXInstalled = 0;
             for (int i = 0; i < myPc.installedGames.Count; i++)
@@ -39,9 +40,11 @@ namespace tdd_oop_concrete_dependency_injection.CSharp.Test
         {
             PowerSupply myPsu = new PowerSupply();
             Computer myPc = new Computer(myPsu);
+            Game game = new Game("Duck Game");
+            Game game2 = new Game("Dragon's Dogma: Dark Arisen");
 
-            myPc.installGame("Duck Game");
-            myPc.installGame("Dragon's Dogma: Dark Arisen");
+            myPc.installGame(game);
+            myPc.installGame(game2);
 
             Assert.That("Playing Duck Game", Is.EqualTo(myPc.playGame("Duck Game")));
             Assert.That("Playing Dragon's Dogma: Dark Arisen", Is.EqualTo(myPc.playGame("Dragon's Dogma: Dark Arisen")));
