b849b5efa0ad118da928b6aa3d542bfd7498f048
Rose Breedveld <rosebreedveld@hotmail.com>
2024-03-20 09:03:18 +0100

    core and extensions done

--- tdd-oop-concrete-dependency-injection.CSharp.Main/Computer.cs
+++ tdd-oop-concrete-dependency-injection.CSharp.Main/Computer.cs
@@ -12,23 +12,29 @@ namespace tdd_oop_concrete_dependency_injection.CSharp.Main
         
         public PowerSupply powerSupply;
 
-        public Computer(PowerSupply powerSupply) {
+        public Computer(PowerSupply powerSupply)
+        {
             this.powerSupply = powerSupply;
         }
 
+        public Computer(PowerSupply psu, List<Game> preInstalledGames) {
+            this.powerSupply = psu;
+            this.installedGames = preInstalledGames;
+        }
+
         public void turnOn() {
-            PowerSupply psu = new PowerSupply();
-            psu.turnOn();
+            //PowerSupply psu = new PowerSupply();
+            this.powerSupply.turnOn();
         }
 
-        public void installGame(string name) {
-            Game game = new Game(name);
+        public void installGame(Game game) {
             this.installedGames.Add(game);
+
         }
 
-        public String playGame(string name) {
+        public String playGame(Game game) {
             foreach (Game g in this.installedGames) {
-                if (g.name.Equals(name)) {
+                if (g.name.Equals(game.name)) {
                     return g.start();
                 }
             }
